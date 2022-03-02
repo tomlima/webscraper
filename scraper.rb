278595338488fa947f9264ab1ae893ccb0029e27
= <=>
2022-03-02 16:51:18 -0300

    Add main script

--- scraper.rb
+++ scraper.rb
@@ -3,5 +3,22 @@ require 'httparty'
 require 'byebug'
 
 def scraper
-    url = 'https://blockwork.cc'
-end
+    url = 'https://www.receptix.uk/br/developer?as=2'
+    unparsed_page = HTTParty.get(url)
+    parsed_page = Nokogiri::HTML(unparsed_page)
+    jobs = Array.new
+    job_listings = parsed_page.css('div.job-item')
+    job_listings.each do |job_listing|
+        job =  {
+            title: job_listing.css('h2.itemHeader').text,
+            link:  job_listing.css('a.jobItemUi').attr("href")
+        }
+        jobs << job
+        puts "Job: #{job[:title]}"
+        puts ""
+        puts "#------------------#"
+        puts ""
+    end
+end 
+
+scraper
